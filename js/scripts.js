34fe5509c8f6e851c403191b017ca0926c803652
Levi McGranahan <levimcg@gmail.com>
2016-09-02 13:47:02 -0400

    Adds new lazy loading for images

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,3 @@
 **  Nice little feature test here instead of using Modernizr
 **  https://24ways.org/2015/putting-my-patterns-through-their-paces/
 */
-
-var doc = document.body || document.documentElement;
-var style = doc.style;
-
-if ( style.webkitFlexWrap == '' || style.msFlexWrap == '' || style.flexWrap == '' ) {
-  doc.className += "supports-flex";
-} else {
-    doc.className += " no-flex";
-}
-
-/*
-**  Hides the images once the DOM is ready so that we can
-**  fade them in later
-*/
-$(document).ready(function() {
-    $(".post-teaser img, .post-content img").hide();
-
-    var $copyrightYear = new Date().getFullYear();
-    $("#js-copyright").html("&copy;" + $copyrightYear);
-});
-
-//Fade in the images once they are loaded
-$(window).on("load", function() {
-    $(".post-teaser img").each(function(index) {
-        $(this).delay(150 * index).fadeIn("slow");
-    });
-});
-
-
-$(window).on("load", function() {
-    $(".post-content img").fadeIn("slow");
-});
